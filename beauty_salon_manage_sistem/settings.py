b843b3afbf9326580f6aa2f4527d4bf7066a6b69
reniboyanova <reni91boyanova@gmail.com>
2022-12-14 13:48:11 +0200

    uninstall sometings

--- beauty_salon_manage_sistem/settings.py
+++ beauty_salon_manage_sistem/settings.py
@@ -1,8 +1,5 @@
 import os
 from pathlib import Path
-import django_heroku
-import dj_database_url
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,5 +134,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
