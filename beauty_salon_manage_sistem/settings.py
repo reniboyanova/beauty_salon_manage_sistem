f981c857503e59e14466cc3c1674e095f1df28fb
reniboyanova <reni91boyanova@gmail.com>
2022-12-14 18:34:09 +0200

    fix

--- beauty_salon_manage_sistem/settings.py
+++ beauty_salon_manage_sistem/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k++)79!c93%(o9*)w!urmlpsx&krgs+54xv$jr+gyc@#b8w6rc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
